3bb57aec89bf91acd33e2853310a95a8e4823b79
Kiryl.Arlou <kirillorlov1205@gmail.com>
2021-11-17 19:05:30 +0300

    Revert "final test version."
    This reverts commit f7d784d6

--- src/main/java/com/stormnet/yandex/framework/actions/diskActions/DiskPageActions.java
+++ src/main/java/com/stormnet/yandex/framework/actions/diskActions/DiskPageActions.java
@@ -11,4 +11,37 @@ import org.openqa.selenium.WebElement;
 
 public class DiskPageActions extends AbstractPageActions {
 
+	@Step("Move file to Files folder")
+	public static void moveFileToFilesFolder(WebElement element) {
+		DownloadsPageActions.waitTillDownloadedFileShown();
+		DownloadsPageActions.invokeContextMenuFile(element);
+		DownloadsPageActions.clickMovingContextButton();
+		FileMovingPopUpActions.selectFileFolder();
+		FileMovingPopUpActions.clickMoveButton();
+		Logger.getLogger().info("File '{}' has been moved to files folder", FileManager.getFile().getName());
+	}
+
+	public static class DiskSideBarMenuActions {
+
+		@Step("Open downloads page")
+		public static void openDownloadsPage() {
+			DiskPage.DiskSideBarMenu.getDownloadsFolderButtonInSideBarMenu().click();
+			DownloadsPageActions.waitTillDownloadsPageOpened();
+			Logger.getLogger().info("Downloads page has been opened");
+		}
+
+		@Step("Open files page")
+		public static void openFilesPage() {
+			waitTillSideBarVisible();
+			DiskPage.DiskSideBarMenu.getFilesFolderButtonInSideBarMenu().click();
+			FilesPageActions.waitTillFilesPageOpened();
+			Logger.getLogger().info("Files page has been opened");
+		}
+
+		public static void waitTillSideBarVisible() {
+			new Waiter().untilVisible(DiskPage.DiskSideBarMenu.getFilesFolderButtonInSideBarMenu(), "Side bar invisible");
+		}
+	}
+
+
 }
